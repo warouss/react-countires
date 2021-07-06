02b9b5a9d0f4f1d22f06582ee1a69995e85f0d84
Walid Arouss <warouss@aliznet.fr>
2021-07-06 17:03:15 +0200

    sort Countries by population

--- src/components/Countries.js
+++ src/components/Countries.js
@@ -4,17 +4,36 @@ import Card from "./Card";
 
 const Countries = () => {
     const [data, setData] = useState([]);
+    const [sortedData, setSortedData] = useState([]);
+    const [playOnce, setPlayOnce] = useState(true);
 
     useEffect(() => {
-        axios.get('https://restcountries.eu/rest/v2/all?fields=name;population;region;region;capital;flag')
-        .then(res => setData(res.data));
-    }, []);
+        if (playOnce) {
+            console.log('call axios');
+            axios.get('https://restcountries.eu/rest/v2/all?fields=alpha3Code;name;population;region;region;capital;flag')
+            .then(res => setData(res.data));
+            setPlayOnce(false);
+        }
+
+        const sortCountries = () => {
+            const countryObj = Object.keys(data).map((i) => data[i]);
+            const sortedArray = countryObj.sort((a,b) => {
+                return b.population - a.population
+            });
+            sortedArray.length = 30;
+            setSortedData(sortedArray);
+        }
+
+        console.log('sortCountries()');
+        sortCountries();
+
+    }, [data, playOnce]);
     
     return (
         <div className="countries">
             <ul className="countries-list">
-                {data.map((country) => (
-                    <Card country={country}/>
+                {sortedData.map((country) => (
+                    <Card key={country.alpha3Code} country={country}/>
                 ))}
             </ul>
         </div>
