d7317beb57e5382dea79014ea395d6b28947e11a
Walid Arouss <warouss@aliznet.fr>
2021-07-06 16:32:14 +0200

    Number format

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,6 +3,10 @@ import React from 'react';
 const Card = (props) => {
     const { country } = props;
 
+    const numberFormat = (x) => {
+        return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, " ");
+    }
+
     return (
         <li className="card">
             <img src={country.flag} alt="flag" />
@@ -10,7 +14,7 @@ const Card = (props) => {
                 <ul>
                     <li>{country.name}</li>
                     <li>{country.capital}</li>
-                    <li>Pop. {country.population}</li>
+                    <li>Pop. {numberFormat(country.population)}</li>
                 </ul>
             </div>
 
