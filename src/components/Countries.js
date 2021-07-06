5ba6f2a7b2f1f1733541fdbf9b8f951f4426be97
Walid Arouss <warouss@aliznet.fr>
2021-07-06 18:42:39 +0200

    Simplification sort + range by population

--- src/components/Countries.js
+++ src/components/Countries.js
@@ -4,30 +4,15 @@ import Card from "./Card";
 
 const Countries = () => {
     const [data, setData] = useState([]);
-    const [sortedData, setSortedData] = useState([]);
-    const [playOnce, setPlayOnce] = useState(true);
     const [rangeValue, setRangeValue] = useState(40);
     const [selectedRadio, setSelectedRadio] = useState('');
     const radios = ['Africa', 'America', 'Asia', 'Europa', 'Oceania'];
 
     useEffect(() => {
-        if (playOnce) {
-            axios.get('https://restcountries.eu/rest/v2/all?fields=alpha3Code;name;population;region;region;capital;flag')
-            .then(res => setData(res.data));
-            setPlayOnce(false);
-        }
-
-        const sortCountries = () => {
-            const countryObj = Object.keys(data).map((i) => data[i]);
-            const sortedArray = countryObj.sort((a,b) => {
-                return b.population - a.population
-            });
-            sortedArray.length = rangeValue;
-            setSortedData(sortedArray);
-        }
-
-        sortCountries();
-    }, [data, rangeValue, playOnce]);
+        axios.get('https://restcountries.eu/rest/v2/all?fields=alpha3Code;name;population;region;region;capital;flag')
+        .then(res => setData(res.data));
+        
+    }, []);
     
     return (
         <div className="countries">
@@ -53,8 +38,10 @@ const Countries = () => {
                 )}
             </div>
             <ul className="countries-list">
-                {sortedData
+                {data
                 .filter((country) => country.region.includes(selectedRadio))
+                .sort((a, b)=> b.population - a.population)
+                .filter((country, index) => index < rangeValue)
                 .map((country) => (
                     <Card key={country.alpha3Code} country={country}/>
                 ))}
