c2e1870493c3bf793f49ea428f37fa08c3f871f2
Walid Arouss <warouss@aliznet.fr>
2021-07-06 17:29:32 +0200

    Filter by region

--- src/components/Countries.js
+++ src/components/Countries.js
@@ -7,6 +7,8 @@ const Countries = () => {
     const [sortedData, setSortedData] = useState([]);
     const [playOnce, setPlayOnce] = useState(true);
     const [rangeValue, setRangeValue] = useState(40);
+    const [selectedRadio, setSelectedRadio] = useState('');
+    const radios = ['Africa', 'America', 'Asia', 'Europa', 'Oceania'];
 
     useEffect(() => {
         if (playOnce) {
@@ -31,9 +33,25 @@ const Countries = () => {
         <div className="countries">
             <div className="sort-container">
                 <input type="range" min="1" max="250" value={rangeValue} onChange={(e) => setRangeValue(e.target.value)}/>
+                <ul>
+                    {radios.map((radio) => {
+                        return (
+                            <li key={radio}>
+                                <input type="radio" value={radio} id={radio}
+                                    checked={radio === selectedRadio}
+                                    onChange={(e) => setSelectedRadio(e.target.value)}
+                                />
+                                <label htmlFor={radio}>{radio}</label>
+                            </li>
+                        )
+                    })}
+                </ul>
+            </div>
             </div>
             <ul className="countries-list">
-                {sortedData.map((country) => (
+                {sortedData
+                .filter((country) => country.region.includes(selectedRadio))
+                .map((country) => (
                     <Card key={country.alpha3Code} country={country}/>
                 ))}
             </ul>
